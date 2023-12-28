88d3e5807f108ddaf1cc60cdab84d2deb68558f3
anilk4 <anilkumark2158@gmail.com>
2023-12-29 00:56:24 +0530

    Implemented search functionality for problems

--- LeetCode_UI/src/Components/Body.jsx
+++ LeetCode_UI/src/Components/Body.jsx
@@ -1,34 +1,69 @@
 import React, { useState, useEffect } from "react";
 import ProblemCard from "./ProblemCard";
 
+const filterData = (searchTxt, problems) => {
+  return problems.filter((prob) =>
+    prob?.title?.toLowerCase()?.includes(searchTxt.toLowerCase())
+  );
+};
+
 const Body = () => {
   const [problems, setProblems] = useState([]);
+  const [filterProblems, setFilterProblems] = useState([]);
+  const [searchTxt, setSearchTxt] = useState('');
 
   useEffect(() => {
     getProblems();
   }, []);
 
-async function getProblems() {
-  try {
-    const response = await fetch('http://localhost:3000/problem/getAll');
-    const data = await response.json();
-    const courseProblems = data.course || [];
-    setProblems(courseProblems); // Set the data to the state
-  } catch (error) {
-    console.error("Error fetching problems:", error);
+  async function getProblems() {
+    try {
+      const response = await fetch('http://localhost:3000/problem/getAll');
+      const data = await response.json();
+      const courseProblems = data.course || [];
+      setProblems(courseProblems);
+      setFilterProblems(courseProblems);
+    } catch (error) {
+      console.error("Error fetching problems:", error);
+    }
   }
-}
-
 
   return (
     <div className="Allproblems">
-  {problems.map((problem) => {
-    return (
-      <ProblemCard {...problem} key={problem._id} />
-    );
-  })}
-</div>
-
+      <input
+        type="text"
+        placeholder="Search Problem"
+        value={searchTxt}
+        className="search-input"
+        onChange={(e) => {
+          setSearchTxt(e.target.value);
+        }}
+      />
+      <button
+        className="search-btn"
+        onClick={() => {
+          const data = filterData(searchTxt, problems);
+          setFilterProblems(data);
+        }}
+      >
+        Search
+      </button>
+      <table className="custom-table">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Title</th>
+            <th>Topics</th>
+            <th>Difficulty</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filterProblems.map((problem) => (
+            <ProblemCard {...problem} key={problem._id} />
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
--- LeetCode_UI/src/Components/ProblemCard.jsx
+++ LeetCode_UI/src/Components/ProblemCard.jsx
@@ -1,11 +1,20 @@
 const ProblemCard = ({ id, title, related_topics, difficulty}) => {
+
+    const Color = (difficulty) => {
+        if (difficulty === 'Hard') {
+            return 'red';
+        } else if (difficulty === 'Medium') {
+            return 'orange';
+        } else return 'green';
+    }
+
     return (
-        <ul>
-            <li>{id}</li>
-            <li>{title}</li>
-            <li>{related_topics}</li>
-            <li>{difficulty}</li>
-        </ul>
+        <tr>
+            <td scope="row" className="link-style">{id}</td>
+            <td className="link-style">{title}</td>
+            <td className="link-style">{related_topics}</td>
+            <td className="link-style">{difficulty}</td>
+        </tr>
     )
 };
 
