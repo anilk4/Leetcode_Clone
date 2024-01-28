efdb795c8b31f803dc4ab5d4b8b30e5176c89828
MuraliAirody <muraliairody@gmail.com>
2024-01-29 00:58:07 +0530

    language stored in local, able to display error message of JAVA code

--- LeetCode_UI/src/Components/Terminal.jsx
+++ LeetCode_UI/src/Components/Terminal.jsx
@@ -1,20 +1,43 @@
+import { useEffect, useState } from "react";
+
 const Terminal = (props) => {
-    const status = props.termi?.data;
+  const status = props.termi?.data;
 
-    if (status?.data?.output === true){
-        console.log("correct code");
+  const language = localStorage.getItem("language");
 
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    if (status?.data?.output === true) {
+      console.log("correct code");
     } else if (status?.data?.result.includes("stderr")) {
-        console.log("Syntax error code")
+      const stderr = JSON.parse(status?.data?.result).stderr;
+
+      if (language === "java") {
+        const match =
+          stderr.match(/Exception in thread ".+" (.+)$/m) ||
+          stderr.match(/error: (.+)\r\n\s+(.+)\r\n/m);
+        //   console.log(match);
+        setError(match ? match[0] : stderr);
+      } else if (language === "py") {
+
+      } else if (language === "js") {
+
+      } else {
+        const match =
+          stderr.match(/Exception in thread ".+" (.+)$/m) ||
+          stderr.match(/error: (.+)\r\n\s+(.+)\r\n/m);
+          setError(match ? match[0] : stderr);
+      }
+      console.log("Syntax error code", error);
     } else {
-        let str = (status?.data?.result)
-        let index = str?.indexOf('\r');
-        let res = str?.substring(0, index);
-        console.log("Else block", res)
+      let str = status?.data?.result;
+      let index = str?.indexOf("\r");
+      let res = str?.substring(0, index);
+      console.log("Else block", res);
     }
-    return (
-        <></>
-    )
-}
+  }, [status, language]);
+  return <>{error}</>;
+};
 
 export default Terminal;
