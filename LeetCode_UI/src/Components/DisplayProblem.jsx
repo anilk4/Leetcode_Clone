fe5510b10e7dc7f39442a4980d26c48682a8020c
MuraliAirody <muraliairody@gmail.com>
2024-01-31 14:04:14 +0530

    Footer design and Split feature

--- LeetCode_UI/src/Components/DisplayProblem.jsx
+++ LeetCode_UI/src/Components/DisplayProblem.jsx
@@ -1,20 +1,25 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Resizable } from 'react-resizable';
 import LeftPanel from "./LeftPanel";
 import RightPanel from "./RightPanel";
-import './Panel/Resizable.css';
 import { useSelector } from "react-redux";
+import { Splitter, SplitterPanel } from "primereact/splitter";
+
+import "primereact/resources/themes/saga-blue/theme.css"; // Add the PrimeReact theme CSS
+import "primereact/resources/primereact.min.css"; // Add the PrimeReact core CSS
+import "primeicons/primeicons.css"; // Add the PrimeIcons CSS
 
 const Display = () => {
   const { id } = useParams();
   const [Data, setData] = useState({});
-  const data = useSelector(store => store.leetCodeProblems.problems);
+  const data = useSelector((store) => store.leetCodeProblems.problems);
 
   console.log("display data: ", data);
 
   async function getData() {
-    const selectedProblem = data.problems?.find(prob => prob.id === parseInt(id));
+    const selectedProblem = data.problems?.find(
+      (prob) => prob.id === parseInt(id)
+    );
     console.log("selectedProblem: ", selectedProblem);
 
     setData(selectedProblem || {});
@@ -24,19 +29,21 @@ const Display = () => {
     getData();
   }, [id, data]);
 
-
   return (
-    <div className="container-fluid">
-      <div className="row">
+    <div className="container-fluid p-0">
+      <Splitter style={{ height: '100%'}}>
         {/* Left side: Problem Description */}
-        <div className="col-md-6 border-right">
-          {Data.id && <LeftPanel data={Data}></LeftPanel>}
-        </div>
+        <SplitterPanel style={{ overflow: 'auto' }}>
+          <div className="border-right p-0 " style={{ wordWrap: "break-word" }}>
+            {Data.id && <LeftPanel data={Data}></LeftPanel>}
+          </div>
+        </SplitterPanel>
+
         {/* Right side: Editor */}
-        <div className="col-md-6">
-          {Data.id && <RightPanel data={Data}/>}
-        </div>
-      </div>
+        <SplitterPanel style={{ overflow: 'auto' }}>
+          <div className="p-0">{Data.id && <RightPanel data={Data} />}</div>
+        </SplitterPanel>
+      </Splitter>
     </div>
   );
 };
--- LeetCode_UI/src/Components/Footer.jsx
+++ LeetCode_UI/src/Components/Footer.jsx
@@ -1,13 +1,64 @@
 import React from "react";
 
 const Footer = () => {
-    return (
-        <footer className="footer mt-5 py-3 bg-light">
-            <div className="container text-center">
-                <p>&copy; 2023 Dummy Leetcode. All Rights Reserved.</p>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="page-footer font-small blue pt-5 " style={{background:"black",color:"white"}}>
+      <div className="container-fluid text-center text-md-left">
+        <div className="row">
+          <div className="col-md-6 mt-md-0 mt-3">
+            <h5 className="text-uppercase">Treat Code</h5>
+            <p>
+              Teat code is a clone of LeetCode; code and enjoy its features.
+            </p>
+          </div>
+
+          <hr className="clearfix w-100 d-md-none pb-0" />
+
+          <div className="col-md-3 mb-md-0 mb-3">
+            <h5 className="text-uppercase">Product</h5>
+            <ul className="list-unstyled">
+              <li>
+                <a className="text-decoration-none " href="#!">About</a>
+              </li>
+              <li>
+                <a className="text-decoration-none " href="#!">Resource</a>
+              </li>
+              <li>
+                <a className="text-decoration-none " href="#!">Support</a>
+              </li>
+              <li>
+                <a className="text-decoration-none " href="#!">Enterprise</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="col-md-3 mb-md-0 mb-3">
+            <h5 className="text-uppercase">Links</h5>
+            <ul className="list-unstyled">
+              <li>
+                <a className="text-decoration-none " href="#!">Privacy Policy</a>
+              </li>
+              <li>
+                <a className="text-decoration-none " href="#!">Security</a>
+              </li>
+              <li>
+                <a className="text-decoration-none " href="#!">API</a>
+              </li>
+             
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="footer-copyright text-center py-3">
+        © 2024 Copyright:Dummy LeetCode. All Rights Reserved.
+        <a href="https://TreatCode.com/" className="text-decoration-none ">
+          {" "}
+          TreatCode.com
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
