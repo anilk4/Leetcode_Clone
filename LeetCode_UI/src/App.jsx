28c0788f9570959c800ac5eabcafc370678fde32
MuraliAirody <muraliairody@gmail.com>
2024-02-01 15:29:22 +0530

    footer fixed

--- LeetCode_UI/src/App.jsx
+++ LeetCode_UI/src/App.jsx
@@ -1,79 +1,137 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React, { lazy, Suspense } from "react";
-import Discuss from './Components/Discuss';
-import { useEffect } from 'react';
-import Error from './Components/Error';
-import Favourites from './Components/Favourites';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import Body from './Components/Body';
-import NavHeader from './Components/NavHeader';
-import Display from './Components/DisplayProblem';
-import Login from './Components/Login';
-import Register from './Components/Register';
-import { userState } from './store/atoms/user';
-import {RecoilRoot,useSetRecoilState} from 'recoil';
-import { BASE_URL } from './config';
-import axios from 'axios';
+import Discuss from "./Components/Discuss";
+import { useEffect } from "react";
+import Error from "./Components/Error";
+import Favourites from "./Components/Favourites";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Body from "./Components/Body";
+import NavHeader from "./Components/NavHeader";
+import Display from "./Components/DisplayProblem";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
+import { userState } from "./store/atoms/user";
+import { RecoilRoot, useSetRecoilState } from "recoil";
+import { BASE_URL } from "./config";
+import axios from "axios";
 
 const LazyProfile = lazy(() => import("./Components/UserProfile"));
 
-function App(){
+const MinHeightWrapper = ({ children }) => {
+  return <div style={{ minHeight: "55vh" }}>{children}</div>;
+};
+
+const App = () => {
   return (
     <RecoilRoot>
-    <Router>
-    <InitUser/>
-      {/* <Header /> */}
-      <NavHeader className="h-25"/>
-      
-      <Routes>
-        <Route path='/' element={<Body  />} />
-        <Route path='/profile' element={<Suspense fallback={<div>Loading...</div>}><LazyProfile /></Suspense>} />
-        <Route path='/discuss' element={<Discuss />} />
-        <Route path='/favourite' element={<Favourites />} />
-        <Route path='/display/:id' element={<Display />} />
-        <Route path='*' element={<Error />} />
-        <Route path='/login' element={<Login/>}></Route>
-        <Route path='/register' element={<Register/>}></Route>
-      </Routes>
-      <Footer />
-    </Router> 
+      <Router>
+        <InitUser />
+        <NavHeader className="h-25" />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <MinHeightWrapper>
+                <Body />
+              </MinHeightWrapper>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <MinHeightWrapper>
+                <Discuss />
+              </MinHeightWrapper>
+            }
+          />
+          <Route
+            path="/favorite"
+            element={
+              <MinHeightWrapper>
+                <Favourites />
+              </MinHeightWrapper>
+            }
+          />
+          <Route
+            path="/display/:id"
+            element={
+              <MinHeightWrapper>
+                <Display />
+              </MinHeightWrapper>
+            }
+          />
+          <Route
+            path="/discuss"
+            element={
+              <MinHeightWrapper>
+                <Discuss />
+              </MinHeightWrapper>
+            }
+          />
+
+          <Route
+            path="*"
+            element={
+              <MinHeightWrapper>
+                <Error />
+              </MinHeightWrapper>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <MinHeightWrapper>
+                <Login />
+              </MinHeightWrapper>
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <MinHeightWrapper>
+                <Register />
+              </MinHeightWrapper>
+            }
+          />
+        </Routes>
+        <Footer />
+      </Router>
     </RecoilRoot>
   );
 };
+
 function InitUser() {
   const setUser = useSetRecoilState(userState);
-  const init = async()=>{
-    try{
-      const res = await axios.get(`${BASE_URL}/account/me`,{
+  const init = async () => {
+    try {
+      const res = await axios.get(`${BASE_URL}/account/me`, {
         headers: {
-          "authorization": "Bearer " + localStorage.getItem("userToken")
-      }
-      })
+          authorization: "Bearer " + localStorage.getItem("userToken"),
+        },
+      });
       console.log(res.data);
-      if(res.data){
+      if (res.data) {
         setUser({
-          userEmail:res.data.username
-        })
-      }
-      else{
+          userEmail: res.data.username,
+        });
+      } else {
         setUser({
-          userEmail:null
-        })
+          userEmail: null,
+        });
       }
-    }
-    catch(e){
-      console.log(e)
+    } catch (e) {
+      console.log(e);
       setUser({
-        userEmail:null
-      })
+        userEmail: null,
+      });
     }
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log("Login");
     init();
-  },[]);
-  return <div></div>
+  }, []);
+  return <div></div>;
 }
 export default App;
--- LeetCode_UI/src/Components/Body.jsx
+++ LeetCode_UI/src/Components/Body.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
-import Pagination from 'react-bootstrap/Pagination';
+import Pagination from "react-bootstrap/Pagination";
 import ProblemCard from "./ProblemCard";
 import { HomeCarousel } from "./HomeCarousel/HomeCarousel";
 import Display from "./DisplayProblem";
 import { Link, useNavigate } from "react-router-dom";
-import './Panel/Resizable.css';
+import "./Panel/Resizable.css";
 import { useDispatch, useSelector } from "react-redux";
 
 import { getProblems } from "../redux/reducers/problemReducer";
@@ -16,9 +16,9 @@ const Body = () => {
   const [searchTxt, setSearchTxt] = useState("");
   const [difficultyLevel, setDifficultyLevel] = useState("All");
   const [currentPage, setCurrentPage] = useState(1);
-  const navigate = useNavigate()   
-  const dispatch = useDispatch()
-  const data = useSelector(store => store.leetCodeProblems.problems)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const data = useSelector((store) => store.leetCodeProblems.problems);
 
   const courseProblems = data.problems || [];
   // console.log("courseProblems : ",courseProblems);
@@ -27,7 +27,6 @@ const Body = () => {
     dispatch(getProblems());
   }, []);
 
-
   useEffect(() => {
     // Check if the data has been fetched before updating state
     if (courseProblems.length > 0) {
@@ -35,7 +34,6 @@ const Body = () => {
       setFilterProblems(courseProblems);
     }
   }, [courseProblems]);
- 
 
   const filterData = (searchTxt, difficultyLevel, problems) => {
     return problems.filter(
@@ -52,14 +50,14 @@ const Body = () => {
 
   const getTextColor = (difficulty) => {
     switch (difficulty) {
-      case 'Easy':
-        return 'green'; // Adjust the color as needed
-      case 'Medium':
-        return 'orange'; // Adjust the color as needed
-      case 'Hard':
-        return 'red'; // Adjust the color as needed
+      case "Easy":
+        return "green"; // Adjust the color as needed
+      case "Medium":
+        return "orange"; // Adjust the color as needed
+      case "Hard":
+        return "red"; // Adjust the color as needed
       default:
-        return 'black'; // Default color
+        return "black"; // Default color
     }
   };
 
@@ -76,30 +74,36 @@ const Body = () => {
   };
 
   return (
-    <div>
-      <div className="mb-3">
+    <div style={{ backgroundColor: "black", color: "white" }}>
+      <div className="">
         <HomeCarousel />
-        <div className="container">
-          <div className="row d-flex justify-content-center"> {/* Center the content */}
+        <div className="container my-3">
+          <div className="row d-flex justify-content-center">
+            {" "}
+            {/* Center the content */}
             <div className="col-md-6">
-              <label htmlFor="difficultyLevel" className="mr-2">Search Problem:</label>
+              <label htmlFor="difficultyLevel" className="mr-2">
+                Search Problem:
+              </label>
               <input
                 className="form-control"
                 type="text"
-                style={{ width: '200px' }}
+                style={{ width: "500px" }}
                 placeholder="Search Problem"
                 value={searchTxt}
                 onChange={(e) => setSearchTxt(e.target.value)}
               />
             </div>
             <div className="col-md-6">
-              <label htmlFor="difficultyLevel" className="mr-2">Difficulty Level:</label>
+              <label htmlFor="difficultyLevel" className="mr-2">
+                Difficulty Level:
+              </label>
               <select
                 id="difficultyLevel"
                 value={difficultyLevel}
                 onChange={(e) => setDifficultyLevel(e.target.value)}
                 className="form-control difficulty-dropdown"
-                style={{ width: '200px' }}
+                style={{ width: "200px" }}
               >
                 <option value="All">All</option>
                 <option value="Easy">Easy</option>
@@ -111,30 +115,40 @@ const Body = () => {
         </div>
       </div>
 
-      <div>
-        <table className="table table-bordered table-striped">
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>Title</th>
-              <th>Difficulty</th>
-              <th>Topics</th>
-            </tr>
-          </thead>
-          <tbody>
-            {currentData.map((problem) => (
-              <tr key={problem.id}>
-                <td>{problem.id}</td>
-                  <td style={{cursor:'pointer'}} onClick={()=>navigate(`/display/${problem.id}`)}>{problem.title}</td>
-                <td style={{ color: getTextColor(problem.difficulty) }}>{problem.difficulty}</td>
-                <td>{problem.related_topics}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+<div className="" style={{ padding: "0 100px"}}>
+  <table className="table table-bordered table-striped">
+    <thead>
+      <tr>
+        <th>Id</th>
+        <th>Title</th>
+        <th>Difficulty</th>
+        <th>Topics</th>
+      </tr>
+    </thead>
+    <tbody>
+      {currentData.map((problem) => (
+        <tr key={problem.id}>
+          <td>{problem.id}</td>
+          <td
+            style={{ cursor: "pointer" }}
+            onClick={() => navigate(`/display/${problem.id}`)}
+          >
+            {problem.title}
+          </td>
+          <td style={{ color: getTextColor(problem.difficulty) }}>
+            {problem.difficulty}
+          </td>
+          <td>{problem.related_topics}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+</div>
 
-      <Pagination className="d-flex justify-content-center"> {/* Center the pagination */}
+
+      <Pagination className="d-flex justify-content-center bg-black p-5 m-0">
+        {" "}
+        {/* Center the pagination */}
         {Array.from({ length: totalPages }, (_, index) => (
           <Pagination.Item
             key={index + 1}
